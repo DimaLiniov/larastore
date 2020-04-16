45ed757ee3501ba9cf7a8ce2d96be9be3cabd6d6
DimaLiniov <dimalinyov@gmail.com>
2020-04-16 11:38:52 +0300

    2.Routes,Controllers

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,9 @@
 |
 */
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', "MainController@index");
+Route::get('/categories', "MainController@categories");
+Route::get('/{category}', "MainController@category");
+Route::get('/mobiles/{product?}', "MainController@product");
 
-Route::get('/categories', function () {
-    return view('categories');
-});
-
-Route::get('/mobiles/iphone_x_64', function () {
-    return view('product');
-});
