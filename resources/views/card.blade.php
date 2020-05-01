2b3c1593ddd0459db85ab834993cf3dabf1448f8
DimaLiniov <dimalinyov@gmail.com>
2020-05-01 11:29:33 +0300

    7.Basket

--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -4,16 +4,16 @@
         </div>
         <img src="http://internet-shop.tmweb.ru/storage/products/iphone_x.jpg" alt="iPhone X 64GB">
         <div class="caption">
-            <h3>iPhone X 64GB</h3>
-            <p>71990 ₽</p>
+            <h3>{{ $product->name }}</h3>
+            <p>{{ $product->price }}</p>
             <p>
-                <a href="{{route('basket')}}" class="btn btn-primary"
-                   role="button">В корзину</a>
-                @isset($category)
-                    {{$category->name}}
-                @endisset
-                <a href="" class="btn btn-default"
+            <form action="{{ route('basket-add', $product) }}" method="post">
+                <button type="submit"  class="btn btn-primary"
+                   role="button">В корзину</button>
+                <a href="{{route('product', [$product->category->code, $product->code])}}" class="btn btn-default"
                    role="button">Подробнее</a>
+                @csrf
+            </form>
         </div>
     </div>
 </div>
